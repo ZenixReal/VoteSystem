6fc3469f519aa2e4592b47b83fdfa27fbe344fe5
ZenixReal <arealzenix.buissnes@hotmail.com>
2025-03-16 15:43:28 +0100

    Initial commit

---
+++ src/main/java/net/exsar/votesystem/VoteSystem.java
@@ -0,0 +1,16 @@
+package net.exsar.votesystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoteSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
